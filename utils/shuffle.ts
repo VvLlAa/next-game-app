99428d8ef1e9196d3005f3914b643e7c54e9d948
Vladislav <vladislavkhramov.r@gmail.com>
2025-04-23 11:47:16 +0300

    added a data type to utils

--- utils/shuffle.ts
+++ utils/shuffle.ts
@@ -1,3 +1,5 @@
-export const shuffle = (array) => {
+import {GameType} from "@/type/type";
+
+export const shuffle = (array : GameType[]) => {
   return [...array].sort(() => Math.random() - 0.5);
 };
