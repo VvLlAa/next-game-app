3f76f7054eb2ce55aece8693211275e2b20dcd60
Vladislav <vladislavkhramov.r@gmail.com>
2025-04-30 19:03:57 +0300

    build configuration

--- src/app/api/steamDeck/fetchSteamDeck.ts
+++ src/app/api/steamDeck/fetchSteamDeck.ts
@@ -1,6 +1,6 @@
 export async function fetchSteamDeck() {
     try {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/steamDeck`, {
+        const res = await fetch(`${process.env.BASE_URL}/api/steamDeck`, {
             next: { revalidate: 0 },
         })
 
